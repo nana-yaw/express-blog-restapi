c4a73b27527e8d5844177e5e3bbafc8b9472b6a0
Edward Osei-Nyarko <edward.osei-nyarko@turntabl.io>
2021-10-15 15:56:57 +0000

    Added validation for routes

---
+++ validations/reset-password-validator.js
@@ -0,0 +1,36 @@
+const { check, validationResult } = require("express-validator")
+
+const validationRules = () => {
+    return [
+        check("oldPassword").trim().isLength({
+            min: 6,
+            max: 16
+        }).withMessage("Old password must be between 6 and 16 characters."),
+        check("newPassword").trim().isLength({
+            min: 6,
+            max: 16
+        }).withMessage("New password must be between 6 and 16 characters."),
+    ]
+}
+
+const validate = (req, res, next) => {
+    const errors = validationRules(req)
+
+    if (errors.isEmpty()) {
+        return next;
+    }
+
+    const resultErrors = []
+    errors.array().map((error) => resultErrors.push({[error.param]: error.mss}))
+
+    resultErrors.push({message: "Action unsuccessful"})
+    resultErrors.push({success: false})
+
+    const errorObject = object.assign({}, ...resultErrors)
+    return res.status(422).json(errorObject)
+}
+
+module.exports = {
+    validationRules,
+    validate
+}
---
+++ validations/video-validator.js
@@ -0,0 +1,36 @@
+const { check, validationResult } = require("express-validator")
+
+const validationRules = () => {
+    return [
+        check("title").trim().isLength({
+            min: 2,
+            max: 256
+        }).withMessage("Title must be between 2 and 256 characters long."),
+        check("videoId").trim().isLength({
+            min: 11,
+            max: 11
+        }).withMessage("Youtube Video ID must be 11 characters long.")
+    ]
+}
+
+const validate = (req, res, next) => {
+    const errors = validationRules(req)
+
+    if (errors.isEmpty()) {
+        return next;
+    }
+
+    const resultErrors = []
+    errors.array().map((error) => resultErrors.push({[error.param]: error.mss}))
+
+    resultErrors.push({message: "Action unsuccessful"})
+    resultErrors.push({success: false})
+
+    const errorObject = object.assign({}, ...resultErrors)
+    return res.status(422).json(errorObject)
+}
+
+module.exports = {
+    validationRules,
+    validate
+}
